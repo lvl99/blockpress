0a343e53717f4a3fe28a8e6933b3701b76b188e7
Matt Scheurich <matt@lvl99.com>
2017-09-17 22:54:37 +0200

    fixed up columns block rendering

--- views/blocks/columns.php
+++ views/blocks/columns.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $block_classes = $configure['element_class'];
 ?>
 
-<div <?php if ( ! empty( $configure['element_id'] ) ) : ?> id="<?php echo $configure['element_id']; ?>" <?php endif; ?> class="layout-block-columns row <?php echo $block_classes; ?>">
+<div <?php if ( ! empty( $configure['element_id'] ) ) : ?> id="<?php echo $configure['element_id']; ?>" <?php endif; ?> class="layout-block-columns <?php echo $block_classes; ?>">
   <?php foreach ( $content['columns'] as $block_index => $block ) :
     $block_view_file = lvl99_acf_page_builder()->locate_block_view( $block['_builder']['block'], $block['_builder']['layout'] );
     echo lvl99_acf_page_builder()->render_view( $block_view_file, [
